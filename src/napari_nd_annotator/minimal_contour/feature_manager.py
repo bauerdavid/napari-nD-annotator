17a7d9391d81b450eacd441c192d16bac167120f
Tejfel <bauerdavid1995@gmail.com>
2023-05-04 13:40:04 +0200

    ensuring temp folder removal, warnings hidden

--- src/napari_nd_annotator/minimal_contour/feature_manager.py
+++ src/napari_nd_annotator/minimal_contour/feature_manager.py
@@ -1,14 +1,17 @@
 import os
 import tempfile
 import atexit
+import warnings
+
 import numpy as np
-import tifffile
 import random
 import string
 import shutil
 from .feature_extractor import FeatureExtractor
 from napari import Viewer
 from typing import Union, Optional
+import glob
+TEMP_SUFFIX = "_nd_annotator"
 
 
 class FeatureManager:
@@ -17,7 +20,8 @@ class FeatureManager:
         self.dims_displayed = None
         self.memmaps: list[Optional[Union[np.ndarray, np.memmap]]] = [None, None]
         self.slices_calculated = dict()
-        self.temp_folder = tempfile.mkdtemp()
+        self.clean_tmp()
+        self.temp_folder = tempfile.mkdtemp(suffix=TEMP_SUFFIX)
         # map layers to file prefix
         self.prefix_map = dict()
         self.viewer: Viewer = viewer
@@ -25,18 +29,24 @@ class FeatureManager:
         atexit.register(self.clean)
 
     def get_features(self, layer, block=True):
-        dims_displayed = tuple(layer._dims_displayed)
-        dims_not_displayed = tuple(layer._dims_not_displayed)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            dims_displayed = tuple(layer._dims_displayed)
+            dims_not_displayed = tuple(layer._dims_not_displayed)
         if layer != self.layer or dims_displayed != self.dims_displayed:
             self.init_file(layer, dims_displayed)
-        idx = tuple(layer._slice_indices[i] for i in range(layer.ndim) if i in dims_not_displayed)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            idx = tuple(layer._slice_indices[i] for i in range(layer.ndim) if i in dims_not_displayed)
         # if not block and not self.slices_calculated[layer][dims_displayed][idx]:
         if not block and not self.feature_extractor.done_mask[idx]:
             raise ValueError("features not calculated for layer %s at %s" % (layer, idx))
         # while not self.slices_calculated[layer][dims_displayed][idx]:
         while not self.feature_extractor.done_mask[idx]:
             pass
-        idx = tuple(layer._slice_indices[i] if i in dims_not_displayed else slice(None) for i in range(layer.ndim))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            idx = tuple(layer._slice_indices[i] if i in dims_not_displayed else slice(None) for i in range(layer.ndim))
         return self.memmaps[0][idx], self.memmaps[1][idx]
 
     def init_file(self, layer, dims_displayed):
@@ -73,7 +83,11 @@ class FeatureManager:
         return os.path.join(self.temp_folder, "tmp_ftrs_%s_%s%s.dat" % (prefix, "_".join(str(d) for d in dims_displayed), suffix))
 
     def start_feature_calculation(self, layer):
-        self.feature_extractor.start_jobs(layer.data, self.memmaps, layer._slice_indices, layer._dims_displayed, layer.rgb)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            slice_indices = layer._slice_indices
+            dims_displayed = layer._dims_displayed
+        self.feature_extractor.start_jobs(layer.data, self.memmaps, slice_indices, dims_displayed, layer.rgb)
 
     @staticmethod
     def random_prefix():
@@ -90,3 +104,9 @@ class FeatureManager:
             pass
         if os.path.exists(self.temp_folder):
             shutil.rmtree(self.temp_folder)
+
+    def clean_tmp(self):
+        temp_dir = tempfile.gettempdir()
+        temp_folders = glob.glob(os.path.join(temp_dir, "%s*%s" % (tempfile.gettempprefix(), TEMP_SUFFIX)))
+        for fold in temp_folders:
+            shutil.rmtree(fold)
